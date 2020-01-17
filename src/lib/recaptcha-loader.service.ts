332f2308f69740f760cb9969d53f36f3d6baa2a7
teskly <im@teskly.com>
2020-01-17 17:49:06 +0300

    0.0.1

---
+++ src/lib/recaptcha-loader.service.ts
@@ -0,0 +1,88 @@
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Inject,
+  Injectable,
+  InjectionToken,
+  Optional,
+  PLATFORM_ID,
+} from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+export const RECAPTCHA_LANGUAGE = new InjectionToken<string>('recaptcha-language');
+export const RECAPTCHA_BASE_URL = new InjectionToken<string>('recaptcha-base-url');
+export const RECAPTCHA_NONCE = new InjectionToken<string>('recaptcha-nonce-tag');
+
+declare global {
+  interface Window {
+    ng2recaptchaloaded: () => void;
+  }
+}
+
+export function loadScript(
+  renderMode: 'explicit' | string,
+  onLoaded: (grecaptcha: ReCaptchaV2.ReCaptcha) => void,
+  urlParams: string,
+  url?: string,
+  nonce?: string,
+) {
+  window.ng2recaptchaloaded = () => {
+    onLoaded(grecaptcha);
+  };
+  const script = document.createElement('script');
+  script.innerHTML = '';
+  const baseUrl = url || 'https://www.google.com/recaptcha/api.js';
+
+  script.src = `${baseUrl}?render=${renderMode}&onload=ng2recaptchaloaded${urlParams}`;
+  if (nonce) {
+    // tslint:disable-next-line:no-any Remove "any" cast once we upgrade Angular to 7 and TypeScript along with it
+    (script as any).nonce = nonce;
+  }
+  script.async = true;
+  script.defer = true;
+  document.head.appendChild(script);
+}
+
+@Injectable()
+export class RecaptchaLoaderService {
+  /**
+   * @internal
+   * @nocollapse
+   */
+  private static ready: BehaviorSubject<ReCaptchaV2.ReCaptcha> = null;
+
+  public ready: Observable<ReCaptchaV2.ReCaptcha>;
+
+  /** @internal */
+  private language: string;
+  /** @internal */
+  private baseUrl: string;
+  /** @internal */
+  private nonce: string;
+
+  constructor(
+    // tslint:disable-next-line:no-any
+    @Inject(PLATFORM_ID) private readonly platformId: any,
+    @Optional() @Inject(RECAPTCHA_LANGUAGE) language?: string,
+    @Optional() @Inject(RECAPTCHA_BASE_URL) baseUrl?: string,
+    @Optional() @Inject(RECAPTCHA_NONCE) nonce?: string,
+  ) {
+    this.language = language;
+    this.baseUrl = baseUrl;
+    this.nonce = nonce;
+    this.init();
+    this.ready = isPlatformBrowser(this.platformId) ? RecaptchaLoaderService.ready.asObservable() : of();
+  }
+
+  /** @internal */
+  private init() {
+    if (RecaptchaLoaderService.ready) {
+      return;
+    }
+    if (isPlatformBrowser(this.platformId)) {
+      const subject = new BehaviorSubject<ReCaptchaV2.ReCaptcha>(null);
+      RecaptchaLoaderService.ready = subject;
+      const langParam = this.language ? '&hl=' + this.language : '';
+      loadScript('explicit', (grecaptcha) => subject.next(grecaptcha), langParam, this.baseUrl, this.nonce);
+    }
+  }
+}
---
+++ src/lib/recaptcha-v3.service.ts
@@ -0,0 +1,142 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, InjectionToken, NgZone, OnDestroy, Optional, PLATFORM_ID } from '@angular/core';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+
+import { loadScript, RECAPTCHA_BASE_URL, RECAPTCHA_NONCE } from './recaptcha-loader.service';
+
+export const RECAPTCHA_V3_SITE_KEY = new InjectionToken<string>('recaptcha-v3-site-key');
+
+export interface OnExecuteData {
+  /**
+   * The name of the action that has been executed.
+   */
+  action: string;
+  /**
+   * The token that reCAPTCHA v3 provided when executing the action.
+   */
+  token: string;
+}
+
+type ActionBacklogEntry = [string, Subject<string>];
+
+/**
+ * The main service for working with reCAPTCHA v3 APIs.
+ *
+ * Use the `execute` method for executing a single action, and
+ * `onExecute` observable for listening to all actions at once.
+ */
+@Injectable()
+export class ReCaptchaV3Service implements OnDestroy {
+  /** @internal */
+  private readonly isBrowser: boolean;
+  /** @internal */
+  private readonly zone: NgZone;
+  /** @internal */
+  private actionBacklog: ActionBacklogEntry[] | undefined;
+  /** @internal */
+  private nonce: string;
+  /** @internal */
+  private baseUrl: string;
+  /** @internal */
+  private grecaptcha: ReCaptchaV2.ReCaptcha;
+
+  /** @internal */
+  private onExecuteSubject: Subject<OnExecuteData>;
+  /** @internal */
+  private onExecuteObservable: Observable<OnExecuteData>;
+
+    public siteKey: BehaviorSubject<string> = new BehaviorSubject(null);
+    private siteKeyObservable: Observable<string> = this.siteKey.asObservable();
+    private sitekeySubscription: Subscription;
+
+  constructor(
+    zone: NgZone,
+    @Optional() @Inject(RECAPTCHA_V3_SITE_KEY) injectedSiteKey: string,
+    // tslint:disable-next-line:no-any
+    @Inject(PLATFORM_ID) platformId: any,
+    @Optional() @Inject(RECAPTCHA_BASE_URL) baseUrl?: string,
+    @Optional() @Inject(RECAPTCHA_NONCE) nonce?: string,
+  ) {
+      this.zone = zone;
+      this.isBrowser = isPlatformBrowser(platformId);
+      this.nonce = nonce;
+      this.baseUrl = baseUrl;
+
+      if (injectedSiteKey) {
+          this.siteKey.next(injectedSiteKey);
+          this.init();
+      }
+
+      this.sitekeySubscription = this.siteKeyObservable
+          .subscribe((newSiteKey: string) => {
+              if (newSiteKey) {
+                  this.init();
+              }
+          });
+  }
+
+    ngOnDestroy(): void {
+        this.sitekeySubscription.unsubscribe();
+    }
+
+    public get onExecute(): Observable<OnExecuteData> {
+        if (!this.onExecuteSubject) {
+            this.onExecuteSubject = new Subject<OnExecuteData>();
+            this.onExecuteObservable = this.onExecuteSubject.asObservable();
+        }
+
+        return this.onExecuteObservable;
+    }
+
+    public execute(action: string): Observable<string> {
+        const subject = new Subject<string>();
+        if (this.isBrowser) {
+            if (!this.grecaptcha) {
+                if (!this.actionBacklog) {
+                    this.actionBacklog = [];
+                }
+
+                this.actionBacklog.push([action, subject]);
+            } else {
+                this.executeActionWithSubject(action, subject);
+            }
+        }
+
+        return subject.asObservable();
+    }
+
+    private executeActionWithSubject(action: string, subject: Subject<string>): void {
+        this.zone.runOutsideAngular(() => {
+            (this.grecaptcha.execute as any)(
+                this.siteKey.getValue(),
+                { action },
+            ).then((token: string) => {
+                this.zone.run(() => {
+                    subject.next(token);
+                    subject.complete();
+                    if (this.onExecuteSubject) {
+                        this.onExecuteSubject.next({ action, token });
+                    }
+                });
+            });
+        });
+    }
+
+    private init() {
+        if (this.isBrowser) {
+            if ('grecaptcha' in window) {
+                this.grecaptcha = grecaptcha;
+            } else {
+                loadScript(this.siteKey.getValue(), this.onLoadComplete, '', this.baseUrl, this.nonce);
+            }
+        }
+    }
+
+    private onLoadComplete = (grecaptcha: ReCaptchaV2.ReCaptcha) => {
+        this.grecaptcha = grecaptcha;
+        if (this.actionBacklog && this.actionBacklog.length > 0) {
+            this.actionBacklog.forEach(([action, subject]) => this.executeActionWithSubject(action, subject));
+            this.actionBacklog = undefined;
+        }
+    }
+}
